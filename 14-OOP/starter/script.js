b695443ab0e3148248566a9915ce3d76faad6e84
beingfarazkhan <faraz.khan14158124@gmail.com>
2023-03-07 13:42:42 +0530

    Object prototype

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -45,3 +45,30 @@ Person.prototype.species = 'Homo Sapiens';
 console.log(jonas.species, matilda.species);
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+// const arr = [10, 12, 13, 321, 2, 121, 423];
+console.log(arr.__proto__);
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
