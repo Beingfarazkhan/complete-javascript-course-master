6aa59ce3fa6a6f78de235aa0e54834305d8dd53e
beingfarazkhan <faraz.khan14158124@gmail.com>
2023-03-08 01:36:27 +0530

    using object.create( ) to implement inheritance

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -316,3 +316,36 @@ console.log(martha);
 martha.introduce();
 martha.calcAge();
 // console.log(`${martha.introduce()} and my age is ${martha.calcAge()}`); wont work
+
+///////////////////////////////////////
+// Inheritance Between "Classes": Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+
+jay.init('Jay', 1990, 'MBA');
+jay.introduce();
+jay.calcAge();
