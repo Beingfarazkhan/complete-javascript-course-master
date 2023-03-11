044da2ecf1100d77766e5d1950132c89934ecbc8
beingfarazkhan <faraz.khan14158124@gmail.com>
2023-03-12 02:34:45 +0530

    chaining promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -136,7 +136,23 @@ setTimeout(() => {
 const getCountryData = country => {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      // console.log(data[0]);
+      const neighbour = data[0].borders[0];
+      console.log(neighbour);
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data[0]);
+      renderCountry(data[0], 'neighbour');
+    });
 };
 
+// getCountryData('germany');
 getCountryData('portugal');
