50c160fbd8afac50b7532faa223f6ae1e26484a1
beingfarazkhan <faraz.khan14158124@gmail.com>
2023-03-13 18:35:33 +0530

    Async await error handling

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -475,6 +475,7 @@ const whereAmI = function () {
 btn.addEventListener('click', whereAmI);
 */
 
+/*
 // Async - Await :
 
 const getPosition = function () {
@@ -483,19 +484,51 @@ const getPosition = function () {
   });
 };
 
-const whereAmI = async function (country) {
-  // fetch(`https://restcountries.com/v3.1/name/${country}`).then(res =>
-  //   console.log(res)
-  // ); //Same result as async await
+const whereAmI = async function () {
+  try {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-  const data = await res.json();
-  console.log(data[0]);
-  renderCountry(data[0]);
+    // Reverse geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!resGeo.ok) throw new Error('Problem getting location data');
+
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
+
+    // Country data
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+    );
+
+    if (!res.ok) throw new Error('Problem getting country');
+
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data[0]);
+  } catch (err) {
+    console.error(`${err} 💥`);
+    renderError(`💥 ${err.message}`);
+  }
 };
+whereAmI();
+whereAmI();
+whereAmI();
+console.log('FIRST');
+
+// try {
+  //   let y = 1;
+//   const x = 2;
+//   y = 3;
+// } catch (err) {
+//   alert(err.message);
+// }
 
 whereAmI('portugal');
 console.log('FIRST');
+
+*/
 
 /////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
