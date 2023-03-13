30b4716a0625f4ba91c160d011fc63a594382901
beingfarazkhan <faraz.khan14158124@gmail.com>
2023-03-14 02:38:49 +0530

    Named Imports and Exports

---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,25 @@
+// Exporting module
+console.log('Exported Module');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `Added ${quantity} ${product}${quantity > 1 ? 's' : ''} to the cart.`
+  );
+};
+
+const totalQuantity = 10;
+const totalPrice = 1270;
+export { cart, totalPrice, totalQuantity as tq };
+
+// Note : Default exports are used when one default value has to be exported, it doesnt have a name
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `Added ${quantity} ${product}${quantity > 1 ? 's' : ''} to the cart.`
+  );
+}
