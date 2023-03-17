7a516df28029177edfc2bb85d8579335ca21c779
Faraz Khan <faraz.khan14158124@gmail.com>
2023-03-18 01:14:53 +0530

    Refactoring Part2 + fractional library

--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -1,13 +1,15 @@
 // import icons from '../img/icons.svg' // for parcel v1
 import icons from 'url:../../img/icons.svg';
 
+import { Fraction } from 'fractional';
+
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
   render(data) {
     this.#data = data;
-    const markup = this.#generateMarkup;
-    this.#clear;
+    const markup = this.#generateMarkup();
+    this.#clear();
     this.#parentElement.insertAdjacentHTML(`afterbegin`, markup);
   }
   #clear() {
@@ -81,41 +83,8 @@ class RecipeView {
           <h2 class="heading--2">Recipe Ingredients</h2>
           <ul class="recipe__ingredient-list">
           ${this.#data.ingredients
-            .map(ingredient => {
-              return `<li class="recipe__ingredient">
-              <svg class="recipe__icon">
-              <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ingredient.quantity}</div>
-              <div class="recipe__description">
-              <span class="recipe__unit">${ingredient.unit}</span>
-              ${ingredient.description}
-              </div>
-              </li>`;
-            })
+            .map(this.#generatedMarkupIngredient)
             .join('')}
-              
-              <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">1000</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">g</span>
-                pasta
-              </div>
-            </li>
-
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">0.5</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">cup</span>
-                ricotta cheese
-              </div>
-            </li>
           </ul>
         </div>
 
@@ -139,6 +108,22 @@ class RecipeView {
           </svg>
           </a>
           </div>`;
+  }
+  #generatedMarkupIngredient(ingredient) {
+    return `<li class="recipe__ingredient">
+        <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+        </svg>
+        <div class="recipe__quantity">${
+          ingredient.quantity
+            ? new Fraction(ingredient.quantity).toString()
+            : ''
+        }</div>
+        <div class="recipe__description">
+        <span class="recipe__unit">${ingredient.unit}</span>
+        ${ingredient.description}
+        </div>
+        </li>`;
   }
 }
 
